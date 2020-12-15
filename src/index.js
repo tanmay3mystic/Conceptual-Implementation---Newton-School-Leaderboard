8c8a90c2767bee00aa27b7f3e323fb6f0ffd6c43
Tanmay Rathod <tanmay.3mystic@gmail.com>
2020-12-16 01:13:59 +0530

    hhg

--- src/index.js
+++ src/index.js
@@ -12,6 +12,21 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 // your code goes here
+const isValid = (obj)=>obj!=null || obj!=undefined ; 
+
+const middleWare = (req,res,next)=>{
+    let {limit , offset} = req.query;
+    req.query.limit = Number(limit)?Number(limit):20;
+    req.query.offset = Number(offset)?Number(offset):0;
+    next();
+}
+app.use(middleWare);
+
+app.get("/topRankings",(req,res)=>{
+    let {limit , offset} = req.query;
+    const tempData = [...data];
+    res.send(tempData.splice(offset,limit));
+})
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
